8237d3e4d3d2a8c668dee41aa78539afb44c13bd
João Rosa <joao.junior@zflow.com.br>
2021-06-14 13:51:08 -0400

    second commit

---
+++ src/test/java/com/rosajr/springmongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosajr.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
