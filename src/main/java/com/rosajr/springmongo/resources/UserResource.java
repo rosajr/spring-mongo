ab4201c2fdc83a65a1968cdbc83855afc70842a2
João Rosa <joao.junior@zflow.com.br>
2021-06-14 17:53:40 -0400

    feat: changed user return to dto model

--- src/main/java/com/rosajr/springmongo/resources/UserResource.java
+++ src/main/java/com/rosajr/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.rosajr.springmongo.resources;
 
 import com.rosajr.springmongo.domain.User;
+import com.rosajr.springmongo.dto.UserDTO;
 import com.rosajr.springmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,8 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
-        return ResponseEntity.ok().body(userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok().body(userService.findAll().stream().map(UserDTO::new).collect(Collectors.toList()));
     }
 
 }
