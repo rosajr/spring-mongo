c7ee9e0bbbe44177122bad2572157afe3212e6ec
João Rosa <joao.junior@zflow.com.br>
2021-06-14 17:18:14 -0400

    feat: user find all with database

---
+++ src/main/java/com/rosajr/springmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.rosajr.springmongo.resources;
+
+import com.rosajr.springmongo.domain.User;
+import com.rosajr.springmongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+}
---
+++ src/main/java/com/rosajr/springmongo/service/UserService.java
@@ -0,0 +1,19 @@
+package com.rosajr.springmongo.service;
+
+import com.rosajr.springmongo.domain.User;
+import com.rosajr.springmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
